440cb977a3a5dc95ca380a48ca4fc0af60ff77b9
Egor Poblaguev <egorpoblaguev@egors-mbp.lan>
2019-02-12 22:47:08 -0500

    Made some progress on rolling modifier cards

--- src/app/classes/deck.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Deck } from './deck';
-
-describe('Deck', () => {
-  it('should create an instance', () => {
-    expect(new Deck()).toBeTruthy();
-  });
-});
--- src/app/classes/deck.ts
+++ src/app/classes/deck.ts
@@ -1,5 +1,5 @@
 export class Deck {
-    cards = {
+    public cards = {
         x0: 1,
         '-2': 1,
         '-1': 5,
@@ -13,35 +13,104 @@ export class Deck {
         'r+2': 0,
     };
 
-    getCardTypes() {
+    private cardValue = {
+        x0: -1000,
+        '-2': -2,
+        '-1': -1,
+        '+0': 0,
+        '+1': 1,
+        '+2': 2,
+        '+3': 3,
+        '+4': 4,
+        x2: 1000,
+        'r+1': 1,
+        'r+2': 2,
+    };
+
+    public getCardTypes() {
         return Object.keys(this.cards);
     }
 
-    sum(): number {
-        return Object.values(this.cards).reduce((prev, cur) => prev + cur);
+    public sum(cards = this.cards): number {
+        return Object.values(cards).reduce((prev, cur) => prev + cur);
     }
 
-    cardChance(cardType: string): number {
+    public cardChance(cardType: string): number {
         return Math.round((this.cards[cardType] / this.sum()) * 100);
     }
 
-    reliabilityNegative() {
+    /*
+    private cloneCards(cards = this.cards) {
+        const newCards = new Object();
+        for (const key of Object.keys(cards)) {
+            Object.defineProperty(newCards, key, {});
+            newCards[key] = cards[key];
+        }
+        return newCards;
+    }
+    */
+
+    public reliabilityNegative_old() {
         return Math.round((this.cards.x0 + this.cards['-2'] + this.cards['-1']) * 100 / this.sum());
     }
 
-    reliabilityZero() {
-        return Math.round((this.cards['+0'] * 100 / this.sum()));
+    private getReliability(cards = this.cards, rollingValue = 0, compareFunc: (x: number) => boolean) {
+        /*
+        let compareFunc: (x: number) => boolean;
+
+
+        if (compare === 0) {
+            compareFunc = (x: number) => x === 0;
+        } else if (compare < 0) {
+            compareFunc = (x: number) => x < 0;
+        } else if (compare > 0) {
+            compareFunc = (x: number) => x > 0;
+        }
+        */
+
+       let probability = 0;
+
+       for (const cardType of Object.keys(cards)) {
+           if (cardType.startsWith('r') && cards[cardType] > 0) {
+               const sum = this.sum(cards);
+               const newCards = Object.assign({}, cards);
+               newCards[cardType] -= 1;
+
+               let tempProbability = (sum - cards[cardType]) / sum;
+               tempProbability *= this.getReliability(newCards, this.cardValue[cardType], compareFunc) / (sum - 1);
+               probability += tempProbability;
+           } else if (compareFunc(rollingValue + this.cardValue[cardType])) {
+               probability += cards[cardType] / this.sum(cards);
+           }
+       }
+
+       return probability;
+
     }
 
-    reliabilityPositive() {
-        return Math.round((this.cards.x2 + this.cards['+3'] + this.cards['+2'] + this.cards['+1']) * 100 / this.sum());
+    public reliabilityNegative() {
+        const compareFunc = (x: number) => x < 0;
+        const probability = this.getReliability(this.cards, 0, compareFunc);
+        return probability * 100;
     }
 
-    addCard(cardType: string) {
+    public reliabilityZero() {
+        const compareFunc = (x: number) => x === 0;
+        const probability = this.getReliability(this.cards, 0, compareFunc);
+        return probability * 100;
+    }
+
+    public reliabilityPositive() {
+        const compareFunc = (x: number) => x > 0;
+        const probability = this.getReliability(this.cards, 0, compareFunc);
+        return probability * 100;
+    }
+
+    public addCard(cardType: string) {
         this.cards[cardType]++;
     }
 
-    removeCard(cardType: string) {
+    public removeCard(cardType: string) {
         if (this.cards[cardType] > 0) { this.cards[cardType]--; }
     }
 }
