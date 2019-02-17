70d17d12678af8322412b2321dd51062a54598d6
epoblagu <egorpoblaguev@gmail.com>
2019-02-17 01:29:44 -0500

    Changed title...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Deck } from './classes/deck';
 import Utils from './utils';
 
@@ -8,13 +8,19 @@ import Utils from './utils';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = Math.random() < 0.5 ? 'Gloomhaven Pork Calculator' : 'Gloomhaven Perk Calculator';
+export class AppComponent implements OnInit {
+  title = 'Gloomhaven Perk Calculator';
   deck = new Deck();
 
   // Table data
   mainTableDataSource = this.deck.getCardTypes();
   displayedColumns: string[] = ['card', 'count', 'percent', 'percent-bar'];
+
+  ngOnInit() {
+    setInterval(() => {
+      this.title = Math.random() < 0.25 ? 'Gloomhaven Pork Calculator' : 'Gloomhaven Perk Calculator';
+    }, 1000);
+  }
 
   updateDeck(cards) {
     this.deck.cards = Utils.clone(cards);
