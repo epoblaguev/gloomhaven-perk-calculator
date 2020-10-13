b201cf5ef7feb5798d65fec8fd7c139272822e1c
Egor Poblaguev <egorpoblaguev@gmail.com>
2020-10-12 22:09:35 -0400

    Clean up app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Deck } from './classes/deck';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { InfoPageComponent } from './modules/info-page/info-page.component';
-import { StatsModules, FaIcons, IconMap } from './classes/consts';
+import { StatsModules, FaIcons } from './classes/consts';
 import { CharacterService } from './services/character.service';
 import { StorageService } from './services/storage.service';
 import { environment } from 'src/environments/environment';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Character } from './classes/character';
 import Utils from './classes/utils';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material/icon';
 
 
 @Component({
@@ -27,9 +25,7 @@ export class AppComponent implements OnInit {
   public statsModules = StatsModules;
   public showDeckModifiers = true;
 
-  public character: Character;
-  public characters: Character[];
-  public subscriptions = new Subscription();
+  public character$: Observable<Character>;
 
   constructor(public bottomSheet: MatBottomSheet, public charService: CharacterService, public storageService: StorageService) {
 
@@ -43,8 +39,7 @@ export class AppComponent implements OnInit {
 
     this.charService.selectCharacter(storageService.getSelectedChar());
 
-    this.subscriptions.add(this.charService.characters$.subscribe(val => this.characters = val));
-    this.subscriptions.add(this.charService.character$.subscribe(val => this.character = val));
+    this.character$ = this.charService.character$;
   }
 
   openBottomSheet(infoType: string): boolean {
@@ -57,7 +52,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnDestroy() {
-    this.subscriptions.unsubscribe();
   }
 
   updateDeck(cards) {
